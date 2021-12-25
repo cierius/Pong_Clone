59ba60718326eb6ec58c8a18ba6ebe76414f78a4
Kanin (Laptop) <kanin.mann00@gmail.com>
2021-12-25 13:19:30 -0500

    Ball movement with scoreboard GUI setup, no function as of yet - Laptop

--- Pong_Clone/Assets/Scripts/Ball_Controller.cs
+++ Pong_Clone/Assets/Scripts/Ball_Controller.cs
@@ -7,6 +7,9 @@ public class Ball_Controller : MonoBehaviour
     
     private const int SPEED = 5;
 
+    private GameObject[] score = new GameObject[2];
+
+    private Rigidbody2D ballRB;
     private GameObject ball;
     private bool flipDir = false; //Left = true, right = false
     private Vector3 movement = new Vector3(1, 0, 0);
@@ -14,17 +17,24 @@ public class Ball_Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if(GameObject.Find("Ball")){
-            ball = GameObject.Find("Ball");
-        }
+        score[0] = GameObject.Find("Score/Left");
+        score[1] = GameObject.Find("Score/Right");
+        ball = GameObject.Find("Ball");
+        ballRB = GetComponent<Rigidbody2D>();
 
-        float ranNum = Random.Range(0.0f, 360.0f) * Mathf.Deg2Rad;;
-        Debug.Log(ranNum);
+        // Generate random starting trajectory
+        // If traj is not satisfactory re-randomize it until it is
+        Vector3 ranAngle = new Vector3(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+        while(ranAngle.z >= 45 && ranAngle.z <= 135 || ranAngle.z >= 225 && ranAngle.z <= 305){
+            ranAngle = new Vector3(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+        }
+        ball.transform.Rotate(ranAngle);
+
+        // Start the ball on it angle 
+        ballRB.AddForce(ball.transform.right * SPEED, ForceMode2D.Impulse);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //ball.transform.position += movement * Time.deltaTime;
+    bool Goal(bool leftWall){
+        return true;
     }
 }
