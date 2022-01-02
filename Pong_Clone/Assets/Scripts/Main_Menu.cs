89cd2b6ed897e5b1c17bc8b385e3f550cb9e609b
Kanin (desktop) <kanin.mann00@gmail.com>
2022-01-02 16:27:28 -0500

    more menu work to be done

---
+++ Pong_Clone/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Menu : MonoBehaviour
+{
+    // Main menu button manager
+
+    void Awake()
+    {
+        
+    }
+
+    public void singlePlayer()
+    {
+
+    }
+
+    public void localMultiplayer()
+    {
+
+    }
+
+    public void exit()
+    {
+
+    }
+}
